da5f129511f46ec7d6b4d8582822b864e33a037b
Joseph Turian <turian@gmail.com>
2008-09-15 21:37:27 -0400

    Added shuffle galleries

---
+++ shuffle_galleries.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl -w
+
+#$count = 4000;
+$count = 2000;
+#$count = 1000;
+#$count = 500;
+$min_images_per_directory = 5;
+$max_images_per_directory = -1;
+#$min_images_per_directory = 0;
+#$max_images_per_directory = 20;
+
+$count = $ARGV[0] if scalar @ARGV == 1;
+
+use List::Util 'shuffle';
+
+@gals = split(/[\r\n]+/, `find . -type d -follow`);
+@gals = shuffle(@gals);
+$lst = "";
+foreach $g (@gals) {
+	next if -e "$g/BLOCKED";	# Skip blocked galleries
+    $g =~ s/"/\\"/g;
+    $g = "\"$g\"";
+	$cmd = "ls $g/*jpg 2> /dev/null";
+
+	$files = `$cmd`;
+	@thisf = split(/\S+/, $files);
+	next if scalar @thisf < $min_images_per_directory;
+	next if (scalar @thisf > $max_images_per_directory && $max_images_per_directory > 0);
+
+	$lst = $lst . $files;
+
+	@allf = split(/\S+/, $lst);
+	last if scalar @allf > $count;
+}
+print $lst;
