68c43944ad857fcc43af924eac1d1384ef51b482
Joseph Turian <turian@gmail.com>
2009-10-16 02:43:53 -0400

    Small change to remove-nonascii-characters.pl. Don't assume STDIN is in utf8

--- remove-nonascii-characters.pl
+++ remove-nonascii-characters.pl
@@ -2,7 +2,7 @@
 #
 # Remove non-ASCII characters
 
-binmode(STDIN, ":utf8");
+#binmode(STDIN, ":utf8");
 binmode(STDOUT, ":utf8");
 while(<>) {
     s/[^[:ascii:]]+//g;   # get rid of non-ASCII characters
