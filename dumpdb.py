162c97675e7c60fdec42ac6b1732525be18db356
Joseph Turian <turian@gmail.com>
2010-09-27 16:59:01 -0400

    Added port and hostname options

--- dumpdb.py
+++ dumpdb.py
@@ -14,10 +14,12 @@ from optparse import OptionParser
 
 parser = OptionParser()
 parser.add_option("-c", "--collection", dest="collection", help="collection name")
-parser.add_option("-d", "--database", dest="database", help="databse name")
+parser.add_option("-d", "--database", dest="database", help="database name")
+parser.add_option("-p", "--port", dest="port", help="port number for mongodb", type="int")
+parser.add_option("--hostname", dest="hostname", help="hostname for mongodb")
 (options, args) = parser.parse_args()
 
-collection = common.mongodb.collection(DATABASE=options.database, name=options.collection)
+collection = common.mongodb.collection(DATABASE=options.database, name=options.collection, PORT=options.port, HOSTNAME=options.hostname)
 for doc in common.mongodb.findall(collection, matchfn=lambda doc: True):
     common.json.dump(doc, sys.stdout, indent=4)
 #    print string.strip(doc["content"].encode("utf-8"))
