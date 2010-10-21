81398ecb78a05948226889bcd60af7510979a5e1
Joseph Turian <turian@gmail.com>
2010-10-21 21:53:46 +0000

    vowpal-to-libsvm.py now works online

--- vowpal-to-libsvm.py
+++ vowpal-to-libsvm.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
 """
 Convert a vowpal-wabbit file in stdin to libsvm.
+
+USAGE:
+    ./vowpal-to-libsvm.py < file.txt > file.libsvm
 """
 
 import string, sys
 
-fc = 1
-fmap = {}
+from common.idmap import IDmap
+from common.file import myopen
+
+fmap = IDmap(allow_unknown=False)
 
 for l in sys.stdin:
     toks = string.split(l)
     print toks[0],
     assert toks[1] == "|features"
+    newfeats = []
     for tok in toks[2:]:
         feat, val = string.split(tok, sep=":")
-        if feat not in fmap:
-            fmap[feat] = fc
-            fc += 1
-        print "%d:%s" % (fmap[feat], val)
+        if not fmap.exists(feat): fmap.add(feat)
+        newfeats.append((fmap.id(feat), val))
+    newfeats.sort()
+    for feat, val in newfeats:
+        print "%d:%s" % (feat, val),
+    print
