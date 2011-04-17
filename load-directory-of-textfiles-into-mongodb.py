5ffd120a7dd469e15563ea61e7ab842d3fe8e9e8
Joseph Turian <turian@gmail.com>
2011-04-17 20:03:22 +0000

    Improvements to load-directory-of-textfiles-into-mongodb.py

--- load-directory-of-textfiles-into-mongodb.py
+++ load-directory-of-textfiles-into-mongodb.py
@@ -12,41 +12,46 @@ import string
 import termextract.train
 
 from common.stats import stats
+import common.mongodb
 
 from optparse import OptionParser
 
 import os, os.path
 
 parser = OptionParser()
-parser.add_option("-d", "--directory", dest="directory", help="directory that contains all documents (text files)", type="string")
+parser.add_option("-i", "--input-directory", dest="directory", help="directory that contains all documents (text files)", type="string")
 parser.add_option("-c", "--collection", dest="collection", help="collection name")
 parser.add_option("-d", "--database", dest="database", help="database name")
 parser.add_option("-p", "--port", dest="port", help="port number for mongodb", type="int")
-parser.add_option("-f", "--field", dest="field", help="field name to use for loaded text", type="string", default="text")
+parser.add_option("-f", "--field", dest="field", help="field name to use for loaded text (default=text)", type="string", default="text")
 parser.add_option("--hostname", dest="hostname", help="hostname for mongodb")
 (options, args) = parser.parse_args()
 
-def getdoc():
+assert options.database is not None
+assert options.collection is not None
+
+if __name__ == "__main__":
+    collection = common.mongodb.collection(DATABASE=options.database, name=options.collection, PORT=options.port, HOSTNAME=options.hostname)
+    print "%s starts with %d docs" % (collection, collection.count())
     for (dirpath, dirnames, filenames) in os.walk(options.directory, followlinks=True):
         if len(filenames) > 0:
-            print >> sys.stderr, "Training on %d files in %s" % (len(filenames), dirpath)
+            print >> sys.stderr, "Loading %d files in %s" % (len(filenames), dirpath)
         for f in filenames:
             f = os.path.join(dirpath, f)
 #            print f
-            doc = open(f).read()
+            txt = open(f).read()
+            doc = {}
+            doc["_id"] = f
             try:
-                yield doc.decode("utf-8")
-            except:
-                print >> sys.stderr, "Could not decode to UTF-8 this file %s" % f
+                doc[options.field] = txt.decode("utf-8")
+#                print doc
+            except Exception, e:
+                print >> sys.stderr, "Could not decode to UTF-8 this file %s" % f, type(e), e
 #                print >> sys.stderr, "Could not decode to UTF-8 this file %s: %s" % (f, repr(doc))
+            try:
+                collection.save(doc)
+#                print "%s now has %d docs" % (collection, collection.count())
+            except Exception, e:
+                print >> sys.stderr, "Could not save to mongodb this file %s" % f, type(e), e
 
-#for d in getdoc():
-#    print len(d), repr(d)[:500]
-
-print >> sys.stderr, "Saving term statistics to %s" % options.name
-print >> sys.stderr, stats()
-termextract.train.compute_term_statistics(options.name, getdoc())
-
-print >> sys.stderr, "Let's threshold the vocabulary, while we're at it"
-print >> sys.stderr, stats()
-termextract.train.threshold(options.name)
+    print "%s now has %d docs" % (collection, collection.count())
