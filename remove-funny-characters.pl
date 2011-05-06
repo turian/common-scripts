8b6af0373f8d57adf9bfa48b230acfdd54e40a10
Joseph Turian <turian@gmail.com>
2011-05-06 06:22:38 +0000

    remove-funny-characters.pl now removes form-feed

--- remove-funny-characters.pl
+++ remove-funny-characters.pl
@@ -2,5 +2,6 @@
 # Remove any funny character
 while(<>) {
     s/([^\s\w\d\/\.\-\{\}\\\:\@\[\]\(\)\+\`\&\'\>\<\$\=\_\#\!\"\!\|\@\#\$\%\^\&\*\~\?\,\.\;])//g;
+    s/\x0c//g;  # Remove form-feed, it trips up Python's lxml parser
     print;
 }
